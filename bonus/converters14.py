4ca18cde510931859d0c62e670e81b9eb2132a29
wlassits <wlagyorgy@gmail.com>
2023-08-26 17:20:51 +0200

    Implement code experiments feet_inches gui.

--- bonus/converters14.py
+++ bonus/converters14.py
@@ -1,4 +1,7 @@
 def convert(feet, inches):
-    meters = feet * 0.3048 + inches * 0.0254
+    try:
+        meters = feet * 0.3048 + inches * 0.0254
+    except TypeError:
+        return "Wrong params"
     # return f"{feet} feet and {inches} inches is equal to {meters} meters"
     return meters
--- day_2/gui.py
+++ day_2/gui.py
@@ -7,10 +7,12 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
+close_button = sg.Button("Close")
 app_title = "My ToDo app"
 
+layout = [[label], [input_box, add_button], [list_box, edit_button], [close_button]]
 window = sg.Window(app_title,
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=layout,
                    font=('Helvetica', 20))
 while True:
     event, values = window.read()
@@ -36,6 +38,9 @@ while True:
             print(todo_to_edit)
         case 'todos':
             window['todo'].update(value=values['todos'][0])
+        case 'Close':
+            window.close()
         case sg.WIN_CLOSED:
             break
+print("Bye")
 window.close()
